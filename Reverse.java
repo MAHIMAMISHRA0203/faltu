91a6d88d8b0ef21f9f340b0dea4eae91c2c80921
MAHIMAMISHRA0203 <mishramahima2004@gmail.com>
2026-01-18 11:05:31 +0530

    reverse file added

---
+++ Reverse.java
@@ -0,0 +1,10 @@
+package dsa_practice.StringsANdSearch;
+
+
+public class Reverse {
+	public static void main(String args[]) {
+		StringBuilder s=new StringBuilder("Hello");
+		s.reverse();
+		System.out.println(s);
+	}
+}
